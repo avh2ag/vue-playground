e3ec5824ed4861a4579b5a41109285bb637fb9ef
alex harasty <alex.harasty@gmail.com>
2023-06-19 19:12:51 -0400

    formatting code

--- src/plugins/DataServicePlugin.js
+++ src/plugins/DataServicePlugin.js
@@ -1,13 +1,11 @@
-
 import { provide } from 'vue';
 import DataService from '../services/DataService';
 
 const DataServicePlugin = {
-    install() {
-      const dataService = new DataService();
-      // const dataServiceInstance = reactive(dataService);
-      provide('dataService', dataService);
-    },
-  };
-  
-  export default DataServicePlugin;
+  install() {
+    const dataService = new DataService();
+    provide('dataService', dataService);
+  },
+};
+
+export default DataServicePlugin;
--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 class DataService {
-    async fetchData() {
-        try {
-          const response = await axios.get('https://my.api.mockaroo.com/users.json?key=e6ac1da0');
-          // Process the response data
-          return response.data;
-        } catch (error) {
-          // Handle the error
-          console.error(error);
-        }
-      }
+  async fetchData() {
+    try {
+      const response = await axios.get(
+        'https://my.api.mockaroo.com/users.json?key=e6ac1da0',
+      );
+      // Process the response data
+      return response.data;
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+    }
   }
+}
 
 export default DataService;
